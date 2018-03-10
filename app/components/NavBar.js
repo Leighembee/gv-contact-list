10960f0965494acf0b75b1841c303291ea331ceb
leighembee <leigh.blechman@gmail.com>
2018-03-09 23:21:16 -0500

    a few styling changes

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,26 +2,37 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import RaisedButton from 'material-ui/RaisedButton'
 import Popover from 'material-ui/Popover'
-import Gavel from 'material-ui/svg-icons/action/gavel'
+import { blue500, red500, greenA200 } from 'material-ui/styles/colors'
+import SvgIcon from 'material-ui/SvgIcon'
 import { NavLink } from 'react-router-dom'
 
+const HomeIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+  </SvgIcon>
+)
+
 const style = {
-  margin: 9,
+  margin: 9
 }
 
 const rightButtons = (
   <div>
-    <NavLink to="/"><RaisedButton label="Home" style={style} /></NavLink>
-    <NavLink to="/contacts"><RaisedButton label="ContactList" style={style} /></NavLink>
+    <NavLink to="/contacts"><RaisedButton label="Contacts" style={style} /></NavLink>
+  </div>
+)
+
+const leftButtons = (
+  <div>
+    <NavLink to="/"><HomeIcon style={style} color={red500} hoverColor={greenA200} /></NavLink>
   </div>
 )
 
 const Navbar = () => {
   return (
     <AppBar
-      iconElementLeft={<Gavel style={{ color: 'black', margin: 9 }} />}
+      iconElementLeft={leftButtons}
       style={{ margin: 'auto', color: 'white' }}
-      title="Contact List"
       iconElementRight={rightButtons} />
   )
 }
